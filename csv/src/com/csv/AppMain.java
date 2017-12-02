f87743c09255b0ce774ba672e45cbe199bf086d4
higuchi <higuchi@higuchi-PC>
2017-12-02 18:14:20 +0900

    201712021814

--- csv/src/com/csv/AppMain.java
+++ csv/src/com/csv/AppMain.java
@@ -17,11 +17,22 @@ class AppMain {
       // 1行ずつCSVファイルを読み込む
       while ((line = br.readLine()) != null) {
         String[] data = line.split(",", 0); // 行をカンマ区切りで配列に変換
+        String code = data[3]; //部門コードを出力
+        System.out.println(code);
 
-        for (String elem : data) {
-        	//PrintStream out = new PrintStream(System.out, true, "SHIFT-JIS");
-          System.out.println(elem);
-        }
+        if (code.equals("1") ){
+        	System.out.println("社員コード:" + data[0] + ",部門コード:" + data[1] + ",部署：" + data[2] + ":自分のさじ加減で人を決めます");
+        }else if (code.equals("2") ){
+           	System.out.println("社員コード:" + data[0] + ",部門コード:" + data[1] + ",部署：" + data[2] + ":雑用的なことをしています");
+        }else if (code.equals("3") ){
+           	System.out.println("社員コード:" + data[0] + ",部門コード:" + data[1] + ",部署：" + data[2] + ":奴隷のように働かされます");
+
+        	}
+
+        //for (String elem : data) {
+          //System.out.println(elem);
+        //}
+
       }
       br.close();
 
